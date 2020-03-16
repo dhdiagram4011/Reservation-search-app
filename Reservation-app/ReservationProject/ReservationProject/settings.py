ea81558b28953fefb6958bd5317a9cf365cc9470
dhdiagram@gmail.com <dhdiagram@gmail.com>
2020-03-16 20:41:00 +0900

    index page create

--- Reservation-app/ReservationProject/ReservationProject/settings.py
+++ Reservation-app/ReservationProject/ReservationProject/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'f%n=uoe%6!=io+njme7$nkqlm2m-^2fusr(h1&tn_v-r^h_$rh'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'ReservationApp',
 ]
 
 MIDDLEWARE = [
