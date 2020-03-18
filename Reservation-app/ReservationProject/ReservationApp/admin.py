bd1a8f0fdce621af959d5548d1dc5e8b09c11584
dhdiagram@gmail.com <dhdiagram@gmail.com>
2020-03-18 20:57:09 +0900

    id field add

--- Reservation-app/ReservationProject/ReservationApp/admin.py
+++ Reservation-app/ReservationProject/ReservationApp/admin.py
@@ -3,35 +3,35 @@ from django.contrib import admin
 from .models import *
 
 class Flight_aircraftOption(admin.ModelAdmin):
-    list_display = ['aircraft_name','aircraft_number']
+    list_display = ['id', 'aircraft_name','aircraft_number']
 admin.site.register(Flight_aircraft, Flight_aircraftOption)
 
 
 
 class Flight_numberOption(admin.ModelAdmin):
-    list_display = ['number']
+    list_display = ['id','number']
 admin.site.register(Flight_number,Flight_numberOption)
 
 
 
 class Flight_sectionOption(admin.ModelAdmin):
-    list_display = ['starting_point','arrival','flight_time']
+    list_display = ['id','starting_point','arrival','flight_time']
 admin.site.register(Flight_section,Flight_sectionOption)
 
 
 
 class PriceOption(admin.ModelAdmin):
-    list_display = ['peak_season_price','low_season_price']
+    list_display = ['id','peak_season_price','low_season_price']
 admin.site.register(Price,PriceOption)
 
 
 
 class Seat_classOption(admin.ModelAdmin):
-    list_display = ['ranking']
+    list_display = ['id','ranking']
 admin.site.register(Seat_class,Seat_classOption)
 
 
 
 class Email_ticketOption(admin.ModelAdmin):
-    list_display = ['method_of_payment','price','ranking','starting_point','arrival','flight_time','seat_number','aircraft_name','number']
+    list_display = ['id','method_of_payment','price','ranking','starting_point','arrival','flight_time','seat_number','aircraft_name','number']
 admin.site.register(Email_ticket,Email_ticketOption)
