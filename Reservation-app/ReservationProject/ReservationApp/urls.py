941512a5a11366cc717067a5ea1ee0f262deb69f
dhdiagram@gmail.com <dhdiagram@gmail.com>
2020-03-18 22:00:41 +0900

    reservation page url routing add

--- Reservation-app/ReservationProject/ReservationApp/urls.py
+++ Reservation-app/ReservationProject/ReservationApp/urls.py
@@ -1,7 +1,10 @@
+# -*- coding: utf-8 -*-
 from django.urls import path
 from .views import *
 
 
 urlpatterns = [
     path('', index, name='index'),
+    path('revstart/', revstart , name='revstart'),
+    path('revsuccess/', revsuccess, name='revsuccess'),
 ]
