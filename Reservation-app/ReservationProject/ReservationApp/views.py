d296926c06d4321f2f9baa52ae5bbe256ecfed52
dhdiagram@gmail.com <dhdiagram@gmail.com>
2020-03-18 21:58:18 +0900

    reservation page add

--- Reservation-app/ReservationProject/ReservationApp/views.py
+++ Reservation-app/ReservationProject/ReservationApp/views.py
@@ -1,6 +1,14 @@
+# -*- coding: utf-8 -*-
 from django.shortcuts import render
 
 
 def index(request):
     return render(request, 'ReservationApp/index.html')
 
+
+def revstart(request):
+    return render(request, 'ReservationApp/rev_start.html')
+
+
+def revsuccess(request):
+    return render(request, 'ReservationApp/rev_success.html')
