14899bd188d634ddfb716a7d05a6036a2f88e789
dhdiagram@gmail.com <dhdiagram@gmail.com>
2020-03-21 15:22:21 +0900

    help_text modify , register model create

--- Reservation-app/ReservationProject/authapp/admin.py
+++ Reservation-app/ReservationProject/authapp/admin.py
@@ -1,4 +1,16 @@
 # -*- coding: utf-8 -*-
 from django.contrib import admin
+from .models import joinMembership
 
-# Register your models here.
+
+class joinMembershipAdmin(admin.ModelAdmin):
+    list_display = ['koreanLastname', 'koreanFirstname', 'englishLastname',
+                    'englishFirstname', 'birthday', 'email',
+                    'email', 'address', 'detailAddress', 'phoneNumber',
+                    'companyName', 'department', 'position', 'fixedlineTelephone']
+
+
+admin.site.register(joinMembership, joinMembershipAdmin)
+
+
+
--- Reservation-app/ReservationProject/authapp/models.py
+++ Reservation-app/ReservationProject/authapp/models.py
@@ -1,4 +1,19 @@
 # -*- coding: utf-8 -*-
 from django.db import models
 
-# Create your models here.
+
+class joinMembership(models.Model):
+    koreanLastname = models.CharField(max_length=1, help_text='국문 이름을 입력해 주세요')
+    koreanFirstname = models.CharField(max_length=3, help_text='국문 성을 입력해 주세요')
+    englishLastname = models.CharField(max_length=10, help_text='영문 이름을 입력해 주세요')
+    englishFirstname = models.CharField(max_length=3, help_text='영문 성을 입력해 주세요')
+    birthday = models.DateField()
+    email = models.EmailField(max_length=50, help_text="해당 이메일 주소로 이메일 티켓이 발송됩니다")
+    address = models.CharField(max_length=50)
+    detailAddress = models.CharField(max_length=50, help_text='상세 주소를 입력해 주세요')
+    phoneNumber = models.CharField(max_length=20, help_text='핸드폰 번호를 입력해 주세요')
+    companyName = models.CharField(max_length=10)
+    department = models.CharField(max_length=10)
+    position = models.CharField(max_length=10)
+    fixedlineTelephone = models.CharField(max_length=20, help_text='유선전화번호를 입력해 주세요')
+
